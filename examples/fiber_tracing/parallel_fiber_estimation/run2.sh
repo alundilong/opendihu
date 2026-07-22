c1499a4a6a2646548d67e3307d487ea20ef7fd6c
alundilong <ymao.mu@gmail.com>
2026-07-22 10:40:50 -0500

    fix: load existing stl file to process

--- examples/fiber_tracing/parallel_fiber_estimation/run2.sh
+++ examples/fiber_tracing/parallel_fiber_estimation/run2.sh
@@ -5,7 +5,7 @@
 # parameters for this script
 use_neumann_bc=false      # (false,true) set to false to use Dirichlet BC (which is better)
 improve_mesh=true         # (false,true) if the Laplacian smoothing and fixing of invalid quadrilaterals should be enabled
-method=stl            # (splines,stl) which input to use, either splines = use "biceps_splines.stl" or stl = use "biceps.surface.pickle"
+method=../biceps_full.stl            # (splines,stl) which input to use, either splines = use "biceps_splines.stl" or stl = use "biceps.surface.pickle"
 refinement=1              # (0,1,2,...) how often to refine the mesh for the Laplace problem, 1=twice as many elements per direction, 2=4x as many elements per direction
 nproc=1                   # (1,8,64,...) number of processes, should be 8^i
 m=2                       # (0,1,2,...) parameter m of fine grid fibers, a higher value means more fibers, use "compute_sizes.py" to get an estimate
@@ -14,7 +14,7 @@ m=2                       # (0,1,2,...) parameter m of fine grid fibers, a highe
 rm -rf out
 
 # run program with MPI
-mpirun -n $nproc --oversubscribe ./generate ../settings_generate2.py \
+mpirun -n $nproc --oversubscribe ./generate ../settings_generate.py \
   --input_filename_or_splines_or_stl $method \
   --refinement_factor $refinement \
   --improve_mesh $improve_mesh \
